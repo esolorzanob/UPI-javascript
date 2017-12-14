afff113eff3370fb97d433c5646bdef840909557
Esteban <esolorzano@renovatiocloud.com>
2017-12-13 20:43:06 -0600

    mundial arreglado

--- mundial/js/partidos.js
+++ mundial/js/partidos.js
@@ -5,16 +5,26 @@ var solicitud = {
     metodo: "selectPais"
 }
 $.ajax({
-    url: "http://api.football-data.org/v1/competitions/467/fixtures",
-    headers: {
-        "X-Auth-Token": "29de3e65e4d84912ad46d8334beacc7c",
-        "X-Response-Control": "minified"
-    },
+    url: "../php/paises.php",
+    data: solicitud,
+    method: 'POST',
     error: function (xhr) {
         alert("An error occured: " + xhr.status + " " + xhr.statusText);
     },
-    success: function (partidos) {
-
-        console.log(partidos);
+    success: function (pais) {
+        $.ajax({
+            url: "http://api.football-data.org/v1/competitions/467/fixtures",
+            headers: {
+                "X-Auth-Token": "29de3e65e4d84912ad46d8334beacc7c",
+                "X-Response-Control": "minified"
+            },
+            error: function (xhr) {
+                alert("An error occured: " + xhr.status + " " + xhr.statusText);
+            },
+            success: function (partidos) {
+                console.log(pais);
+                console.log(partidos);
+            }
+        });
     }
 });
